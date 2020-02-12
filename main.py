08b696cd223fb3f26e2c4ae9d2ca2b28d02034b6
Steven <60404094+sltayl19@users.noreply.github.com>
2020-02-12 14:32:45 -0500

    adsf

--- main.py
+++ main.py
@@ -5,14 +5,18 @@ window = pyglet.window.Window() # create the window
 # label = pyglet.text.Label('Hello, world', x = 200, y = 200)
 
 # Create a sprite
-ball_image = pyglet.image.load('assets/hero/Old hero.png')
-ball = pyglet.sprite.Sprite(ball_image, x=50, y=50)
+img = pyglet.image.load('assets/hero/Old hero.png')
+cave = pyglet.image.load('assets/gfx/cave.png')
 
 # Start the event loop
+def update(dt):
+    pass
+
 @window.event
 def on_draw():
     window.clear()
-    ball.x += 1
-    ball.draw()
+    img.blit(100, 200)
+    img.blit(50,300)
 
+pyglet.clock.schedule(update)
 pyglet.app.run()
