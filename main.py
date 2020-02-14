5a1585a89fa1b4ab99179b6b99a0544566a950f0
Steven <60404094+sltayl19@users.noreply.github.com>
2020-02-14 14:33:32 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,35 @@
 import pyglet # import the library
 window = pyglet.window.Window() # create the window
 
-pyglet.window.key.KeyStateHandler()
-keys = pyglet.window.key.KeyStateHandler()
-
-# Create some text
-# label = pyglet.text.Label('Hello, world', x = 200, y = 200)
-
 # Create a sprite
 img = pyglet.image.load('assets/hero/Old hero.png')
-cave = pyglet.image.load('assets/hero/sliced/idle-1.png')
+idle = pyglet.image.load('assets/hero/sliced/idle-1.png')
 log = pyglet.image.load('assets/gfx/log.png')
 kick = pyglet.image.load('assets/hero/sliced/kick.png')
 jump = pyglet.image.load('assets/hero/sliced/jump-1.png')
 
+keys = pyglet.window.key.KeyStateHandler()
+window.push_handlers(keys)
 
-
+spr = pyglet.sprite.Sprite(img, x = 300, y = 300)  
+spr.draw()
 
 # Start the event loop
 def update(dt):
-    pass
-
-
+    if keys[pyglet.window.key.SPACE]:
+    print("SpaceBar is pressed!")
+    if keys[pyglet.window.key.UP]:
+    print("Up key pressed!")
 
 
 @window.event
 def on_draw():
     window.clear()
     img.blit(100, 200)
-    cave.blit(50,300)
     log.blit(400,200)
     kick.blit(300,100)
+    jump.blit (200,100)
     
-    
+
 pyglet.clock.schedule(update)
 pyglet.app.run()
