ae350f14953baf42701b7bd6b5831b6ede9318de
Steven <60404094+sltayl19@users.noreply.github.com>
2020-02-18 13:22:14 -0500

    d

--- main.py
+++ main.py
@@ -8,18 +8,23 @@ log = pyglet.image.load('assets/gfx/log.png')
 kick = pyglet.image.load('assets/hero/sliced/kick.png')
 jump = pyglet.image.load('assets/hero/sliced/jump-1.png')
 
+
+
+
+
+
 keys = pyglet.window.key.KeyStateHandler()
 window.push_handlers(keys)
 
-spr = pyglet.sprite.Sprite(img, x = 300, y = 300)  
+spr = pyglet.sprite.Sprite(img, x = 200, y = 200)
 spr.draw()
 
 # Start the event loop
 def update(dt):
-    if keys[pyglet.window.key.SPACE]:
-    print("SpaceBar is pressed!")
-    if keys[pyglet.window.key.UP]:
-    print("Up key pressed!")
+ if keys[pyglet.window.key.LEFT]:
+  spr.x -= 1
+ if keys[pyglet.window.key.RIGHT]:
+  spr.x += 1
 
 
 @window.event
